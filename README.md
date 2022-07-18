c9194ea4082d563e894cc84f11af08cb8025966d
ponkan1219 <kiyotatakeshi.work@gmail.com>
2022-07-18 11:01:46 +0900

    impl CRUD, add README

---
+++ README.md
@@ -0,0 +1,18 @@
+# nodejs-mongo-sample
+
+## run locally
+
+```shell
+$ node -v      
+
+v16.16.0
+```
+
+```shell
+$ cp .env.sample .env
+
+# write MongoDB Atlas connection information
+$ vi .env
+
+$ nodemon
+```
--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const username = process.env.USER_NAME;
 const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const database = `blog`;
 
 // connecting to MongoDB
-mongoose.connect(`mongodb+srv://${username}:${password}@sandbox.irjo5.mongodb.net/blogUser?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${username}:${password}@${host}/${database}?retryWrites=true&w=majority`)
     .then(() => {
         console.log("Success: Connected to MongoDB")
     })
@@ -24,33 +26,68 @@ const blogSchema = new schema({
     content: String,
 });
 
-const blogModel = mongoose.model("Blog", blogSchema);
+const blogModel = mongoose.model(
+    "blog", // collection name(automatically add 's')
+    blogSchema
+);
 
-app.get("/", async(req, res) => {
+app.get("/blogs", async (req, res) => {
     const allBlogs = await blogModel.find();
-    console.log("all blogs:" , allBlogs)
-    res.send("fetch all blogs")
+    console.log("all blogs:", allBlogs)
+    res.send(allBlogs)
 })
 
-app.get("/blog/:id", async (req, res) => {
-    const blog = await blogModel.findById(req.params.id);
-    console.log("blog:", blog)
-    res.send("blog page")
-})
-
-app.get("/blog/create", (req, res) => {
+app.get("/blogs/create", (req, res) => {
     res.sendFile(__dirname + "/views/blogCreate.html")
 })
 
-app.post("/blog", (req, res) => {
+app.get("/blogs/:id", async (req, res) => {
+    // TODO:
+    // CastError: Cast to ObjectId failed for value "62d4b43509b03db1e611f60" (type string) at path "_id" for model "Blog"
+    const blog = await blogModel.findById(req.params.id);
+    console.log("blog:", blog)
+    if (blog == null) {
+        res.send("no such id blog")
+    } else {
+        res.send(blog)
+    }
+})
+
+app.post("/blogs", (req, res) => {
     console.log("req content", req.body)
     blogModel.create(req.body, (error, savedBlogData) => {
-        if(error){
-            console.log("fail to write data")
-            res.send("fail to post blog data")
+        if (error) {
+            console.log("fail to post blog")
+            res.send("fail to post blog")
         } else {
-            console.log("success to write data")
-            res.send("post blog data")
+            console.log("success to post blog:", savedBlogData)
+            res.send("success to post blog")
+        }
+    })
+})
+
+app.put("/blogs/:id", async (req, res) => {
+    blogModel.updateOne({_id: req.params.id}, req.body).exec((error) => {
+        if (error) {
+            console.log("fail to update blog")
+            res.send("fail to update blog")
+        } else {
+            console.log("success to update blog")
+            res.send("success to update blog")
+        }
+    });
+})
+
+app.delete("/blogs/:id", (req, res) => {
+    const target = req.params.id;
+    console.log("delete:", target)
+    blogModel.deleteOne({_id: target}).exec((error) => {
+        if (error) {
+            console.log("fail to delete")
+            res.send("fail to delete")
+        } else {
+            console.log("success to delete")
+            res.send("success to delete")
         }
     })
 })
